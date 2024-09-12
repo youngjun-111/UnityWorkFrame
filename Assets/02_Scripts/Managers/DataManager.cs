8e55a2f463602cf3d139ee8855e12e21c05779ba
youngjun-111 <jrys7754@gmail.com>
2024-09-12 15:44:13 +0900

    DataManager
    DataManager+Json

--- Assets/02_Scripts/Managers/DataManager.cs
+++ Assets/02_Scripts/Managers/DataManager.cs
@@ -1,12 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class DataManager : MonoBehaviour
+//메모리에서 들고 있는 거를 파일로 변환 할 수 있는 공식
+[Serializable]
+public class Stat
+{
+    public int level;
+    public int hp;
+    public int attack;
+}
+
+[Serializable]
+public class StatData
+{
+    public List<Stat> stats = new List<Stat>();
+}
+
+public class DataManager
 {
     public void Init()
     {
         //경로를 항상 생각해야함
         TextAsset textAsset = Managers.Resources.Load<TextAsset>($"Data/StatData");
+        StatData data = JsonUtility.FromJson<StatData>(textAsset.text);
     }
 }
